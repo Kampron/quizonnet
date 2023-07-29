fa1134db4e133f767d106d0ccc2520a8c8fa0ccf
Kampron <qwequkyei@gmail.com>
2023-07-29 15:07:58 +0000

    fix typo

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,5 @@
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { toast } from 'react-hot-toast';
 
