b59e30bfc08480346c15605f9e0fa64acfce20ef
Kampron <qwequkyei@gmail.com>
2023-08-26 07:59:57 +0000

    dashboard

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -4,61 +4,56 @@ import bcrypt from 'bcryptjs';
 import { connectToDB } from '@/utils/db';
 import { generateToken, verifyToken } from '@/utils/token';
 import sendEmail from '@/utils/sendEmail';
-import { redirect } from 'next/dist/server/api-utils';
 
 const BASE_URL = process.env.NEXTAUTH_URL;
 
 connectToDB();
+
 export const POST = async (request) => {
-  const data = await request.json();
-
-  const isExisting = await User.findOne({ email: data.email });
-
-  if (isExisting) {
-    return new NextResponse('Email registered already', {
-      status: 409,
-    });
-  }
-  const hashedPassword = isExisting
-    ? null
-    : await bcrypt.hash(data.password, 12);
-
-  const newUser = new User({
-    username: data.username,
-    email: data.email,
-    password: hashedPassword,
-  });
-
   try {
+    const data = await request.json();
+
+    const isExisting = await User.findOne({ email: data.email });
+
+    if (isExisting) {
+      return new NextResponse('Email registered already', {
+        status: 409,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 12);
+
+    const newUser = new User({
+      username: data.username,
+      email: data.email,
+      password: hashedPassword,
+    });
+
     await newUser.save();
-  } catch (err) {
-    return new NextResponse(err.message, {
-      status: 500,
-    });
-  }
 
-  const token = generateToken({ user: newUser });
-  try {
+    const token = generateToken({ user: newUser });
+
     await sendEmail({
       to: newUser.email,
       url: `${BASE_URL}/verify?token=${token}`,
       text: 'VERIFY EMAIL',
     });
-  } catch (err) {
-    return new NextResponse(err.message, {
+
+    return new NextResponse('User has been created', {
+      status: 201,
+    });
+  } catch (error) {
+    return new NextResponse(error.message, {
       status: 500,
     });
   }
-
-  return new NextResponse('User has been created', {
-    status: 201,
-  });
 };
 
 export async function verifyWithCredentials(token) {
   try {
     const { user } = verifyToken(token);
     const userExist = await User.findOne({ email: user.email });
+
     if (userExist) {
       return { msg: 'Verify Success!' };
     } else {
